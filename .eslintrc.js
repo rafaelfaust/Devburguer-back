0736a3b77ec653dfe1647ce92549b910177f9479
Webfael <rafaelfaustrodrigues@gmail.com>
2023-03-18 20:41:07 -0300

    First

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["standard", "prettier"],
+  plugins: ["prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    endOfLine: "auto",
+    camelcase: "off",
+    "prettier/prettier": "error",
+  },
+};
