95b2839beb56160645f130ec96a00cc0bdf6654f
Webfael <rafaelfaustrodrigues@gmail.com>
2023-03-19 22:38:50 -0300

    Adicionado controller user

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,27 @@
+/* 
+store  => Cadastar / Adicionar
+index  => Listar varios
+show   => Listar apenas um
+update => Atualizar
+delete => Deletar
+*/
+import { v4 } from 'uuid'
+import User from '../models/User'
+
+class UserController {
+    async store(request, response) {
+        const { name, email, password_hash, admin } = request.body
+
+        const user = await User.create({
+            id: v4(),
+            name,
+            email,
+            password_hash,
+            admin,
+        })
+
+        return response.status(201).json({id: user.id, name, email, admin})
+    }
+}
+
+export default new UserController()
--- src/database/migrations/20230319214422-create-users.js
+++ src/database/migrations/20230319214422-create-users.js
@@ -38,7 +38,7 @@ module.exports = {
         allowNull: false,
       },
 
-      update_at: {
+      updated_at: {
         type: Sequelize.DATE,
         allowNull: false,
       },
