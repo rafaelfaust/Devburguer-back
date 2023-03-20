a3d637ea246fb07f72a2ee2cf96a51f09e3242d8
Webfael <rafaelfaustrodrigues@gmail.com>
2023-03-19 23:33:47 -0300

    integrado Yup

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -7,9 +7,26 @@ delete => Deletar
 */
 import { v4 } from 'uuid'
 import User from '../models/User'
+import * as Yup from 'yup'
 
 class UserController {
     async store(request, response) {
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password_hash: Yup.string().required().min(6),
+            admin: Yup.boolean,
+        })
+
+        try {
+            await schema.isValidSync(request.body)
+        } catch (err) {
+            return response.status(400).json({ error: err.error})
+            
+        }
+
+
         const { name, email, password_hash, admin } = request.body
 
         const user = await User.create({
@@ -20,7 +37,7 @@ class UserController {
             admin,
         })
 
-        return response.status(201).json({id: user.id, name, email, admin})
+        return response.status(201).json({ id: user.id, name, email, admin })
     }
 }
 
