89146dafff8aef9b2c30275cf8929af933b56384
Webfael <rafaelfaustrodrigues@gmail.com>
2023-03-29 13:24:26 -0300

    Categories

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import authMiddlewares from './app/middlewares/auth'
 import SessionController from './app/controllers/SessionController'
 import ProductController from './app/controllers/ProductController'
 import UserController from './app/controllers/UserController'
+import CategoryController from './app/controllers/CategoryController'
 
 
 const upload = multer(multerConfig)
@@ -22,5 +23,7 @@ routes.use(authMiddlewares)
 routes.post('/products', upload.single('file'), ProductController.store)
 routes.get('/products', ProductController.index)
 
+routes.post('/categories', upload.single('file'), CategoryController.store)
+routes.get('/categories', CategoryController.index)
 
 export default routes
